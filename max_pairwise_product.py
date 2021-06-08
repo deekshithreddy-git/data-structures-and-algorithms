e75bb18789ee04d6518a7c5c6e1f4ec3a45a41cc
Deekshith Reddy Doddipalli Muni <40777901+deekshithreddy-git@users.noreply.github.com>
2021-06-08 06:59:22 +0530

    Update max_pairwise_product.py

--- max_pairwise_product.py
+++ max_pairwise_product.py
@@ -4,13 +4,10 @@
 def max_pairwise_product_naive(numbers):
     assert len(numbers) >= 2
     assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
-
     product = 0
-
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             product = max(product, numbers[i] * numbers[j])
-
     return product
 
 # optimized method
@@ -20,7 +17,6 @@ def max_pairwise_product(numbers):
     sorted_list = sorted(numbers, reverse=True)
     max_product = sorted_list[0] * sorted_list[1]
     return max_product
-
 
 if __name__ == '__main__':
     n = int(input())
